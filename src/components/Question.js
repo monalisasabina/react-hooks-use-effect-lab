9619749d69d709acb1fbdb7433c1d652e54f585f
Monalisa Onyango <+>
2024-08-06 02:09:54 +0300

    useEffect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  // add useEffect code.............................................
+  useEffect(() =>{
+    // setTimeout(() =>{
+    //   setTimeRemaining(timeRemaining -1)
+    // },1000)
+    
 
+      if(timeRemaining === 0){
+        setTimeRemaining(10)
+        onAnswered(false)
+        return
+      }
+
+      const timerId =setTimeout(()=>{
+        setTimeRemaining(timeRemaining-1)
+      },1000)
+
+      return function(){
+        clearTimeout(timerId)
+      }
+
+  },[timeRemaining,onAnswered])
+
+  // .............................................................................
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
